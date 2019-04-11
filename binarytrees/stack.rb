3a993f0237a003b8b6f31302719cdd905f2af435
semicolon jeff <palmbeachjeff@gmail.com>
2019-04-11 09:30:17 -0400

    working through stack exercise, not done yet

--- binarytrees/stack.rb
+++ binarytrees/stack.rb
@@ -1,19 +1,39 @@
 class Stack
   class Stack
     def initialize
-      # create ivar to store stack here!
+      # create a var to store stack here!
+      @stack = []
+       
     end
 
     def push(el)
       # adds an element to the stack
+      el << stack 
+      stack
     end
 
     def pop
       # removes one element from the stack
+      @stack.pop
+      return @stack
     end
 
     def peek
       # returns, but doesn't remove, the top element in the stack
+      @stack.first 
     end
   end
 end
+p "--------"
+sandwich = Stack.new
+sandwich.push("bacon")
+sandwich.push("lettuce")
+sandwich.push("tomato")
+p sandwich
+p "--------"
+sandwich.pop
+p sandwich 
+p "--------"
+p sandwich.peek
+p "--------"
+
