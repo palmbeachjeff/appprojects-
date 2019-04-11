ebf4900ab4f3a8d5c70055fbefb102051f663e7b
semicolon jeff <palmbeachjeff@gmail.com>
2019-04-11 10:51:47 -0400

    Map class methods are complete

--- binarytrees/stack.rb
+++ binarytrees/stack.rb
@@ -69,17 +69,26 @@ class Map
 
   def get(key)
     #iterate through sub arrays and find the first value
-    @twodarray.each do |array|
-      if key == array[0]
-
-    end
-
+    underlying_array.each {|pair| return pair[1] if pair[0] == key}
+    nil  
   end
 
   def delete(key)
+    value = get(key)
+    underlying_array.reject {|pair| pair[0] == key}
+    value
   end
 
   def show
+    deep_dup(underlying_array)
+  end
+
+  private
+
+  attr_reader :underlying_array
+
+  def deep_dup(arr)
+    arr.map {|el| el.is_a?(Array) ? deep_dup(el) : el }
   end
 
 end
