2529f2af15471b201bbcf839a58249a81417108a
semicolon jeff <palmbeachjeff@gmail.com>
2019-04-11 09:17:06 -0400

    initial commit for stack project

---
+++ binarytrees/stack.rb
@@ -0,0 +1,19 @@
+class Stack
+  class Stack
+    def initialize
+      # create ivar to store stack here!
+    end
+
+    def push(el)
+      # adds an element to the stack
+    end
+
+    def pop
+      # removes one element from the stack
+    end
+
+    def peek
+      # returns, but doesn't remove, the top element in the stack
+    end
+  end
+end
