ccee3488822384ee2e34f773ac8bb35430eae6c7
semicolon jeff <palmbeachjeff@gmail.com>
2019-04-12 14:19:29 -0400

    inheritance is done

--- AprilTwelfth/ClassInheritance/inheritance.rb
+++ AprilTwelfth/ClassInheritance/inheritance.rb
@@ -1,9 +1,17 @@
 class Employee
-  def initialize
-    @name = name
-    @title = title
-    @salary = salary
+  attr_reader :name, :salary, :title
+  attr_accessor :boss
+
+  def initialize(name, salary, title, boss = nil)
+     @name, @salary, @title = name, salary, title
+    self.boss = boss
+  end
+
+  def boss=(boss)
     @boss = boss
+    @boss.add_employee(self) unless boss.nil?
+
+    boss
   end
 
   def bonus(multiplier)
@@ -14,12 +22,41 @@ end
 
 
 class Manager < Employee
-  def initialize
+  attr_reader :employees 
+
+  def initialize(name, salary, title, boss = nil)
+    super(name, salary, title, boss)
     @employees = []
   end
 
+  def add_employee(subordinate)
+    employees << subordinate
+    
+    subordinate
+  end
+
   def bonus(multiplier)
-    bonus = 
+    bonus = total_salary(@employees) * multiplier
+    bonus
+  end
+
+  protected 
+
+  def total_salary
+    total_salary = 0
+    @employees.each do |employee|
+      if employee.is_a?(Manager)
+        total_salary += employee.salary + employee.total_salary
+      else 
+        total_salary += employee.salary
+      end
+    end
+    total_salary
   end
 
 end
+
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
