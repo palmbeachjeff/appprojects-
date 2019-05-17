1b6494a5c6b73a925dc774e1110b46b06b7039db
semicolon jeff <palmbeachjeff@gmail.com>
2019-05-17 10:02:25 -0400

    game works and is bug free

--- May13/battleship.js
+++ May13/battleship.js
@@ -29,9 +29,11 @@ var model = {
   numShips: 3, 
   shipsSunk: 0,
   shipLength: 3,
-  ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
-          { locations: ["24", "34", "44"], hits: ["", "", ""] },
-          { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+  ships: [
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] },
+    { locations: [0, 0, 0], hits: ["", "", ""] }
+  ],
   fire: function(guess) {
       for (var i = 0; i < this.numShips; i++){
         var ship = this.ships[i];
@@ -59,14 +61,62 @@ var model = {
       }
     }
     return true; 
+  },
+
+  generateShipLocations: function() {
+    var locations;
+    for (var i = 0; i < this.numShips; i++) {
+      do {
+        locations = this.generateShip();
+      } while (this.collision(locations));
+      this.ships[i].locations = locations;
+    }
+  },
+
+  generateShip: function() {
+    var direction = Math.floor(Math.random() * 2);
+    var row; 
+    var col;
+    if (direction === 1){
+      row = Math.floor(Math.random() * this.boardSize);
+      col = Math.floor(Math.random() * (this.boardSize - (this.shipLength + 1)));
+    } else {
+      row = Math.floor(Math.random() * (this.boardSize - this.shipLength + 1));
+      col = Math.floor(Math.random() * this.boardSize);
+    }
+
+    var newShipLocations = [];
+    for (var i = 0; i < this.shipLength; i++){
+      if (direction === 1){
+        newShipLocations.push(row + "" + (col + i));
+      } else {
+        newShipLocations.push((row + i) + "" + col); 
+      }
+    }
+    return newShipLocations; 
+  },
+
+  collision: function(locations) {
+    for (var i = 0; i < this.numShips; i++){
+      var ship = this.ships[i];
+      for (var j = 0; j < locations.length; j++) {
+        if (ship.locations.indexOf(locations[j]) >= 0) {
+          return true; 
+        }
+      }
+    }
+    return false; 
   }
+
+
+
 };
 
 function parseGuess(guess) {
   var alphabet = ["A", "B", "C", "D", "E", "F", "G"]; 
 
   if (guess === null || guess.length !== 2){
-    alert("Oops, please enter a letter and a number on teh board.");
+    alert("Oops, please enter a letter and a number on the board.");
   } else {
     var firstChar = guess.charAt(0);
     var row = alphabet.indexOf(firstChar); 
@@ -100,3 +150,29 @@ var controller = {
   }
 };
 
+function init() {
+  var fireButton = document.getElementById("fireButton");
+  fireButton.onclick = handleFireButton;
+  var guessInput = document.getElementById('guessInput');
+  guessInput.onkeypress = handleKeyPress;
+
+  model.generateShipLocations();
+}
+
+function handleFireButton() {
+  var guessInput = document.getElementById('guessInput');
+  var guess = guessInput.value;
+  controller.processGuess(guess);
+
+  guessInput.value = ""; 
+}
+
+function handleKeyPress(e) {
+  var fireButton = document.getElementById("fireButton");
+  if (e.keyCode === 13) {
+    fireButton.click();
+    return false; 
+  }
+}
+
+window.onload = init; 
